8c90f2a422cb23adb6463d83b0068df3ede4bd7f
qjstuart <quentinqjs@gmail.com>
2023-10-16 13:17:18 +0200

    Added cart button and icon to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import modularMoogsImage from "../../assets/modular-moogs-image.webp";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>React SynthStore</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
       <div className={styles["main-image"]}>
         <img src={modularMoogsImage} alt="Modular synthesizer desk setup" />
