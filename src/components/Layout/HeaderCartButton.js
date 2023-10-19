98caf2990e20af92d12498c001c5aea4054fa1f6
qjstuart <quentinqjs@gmail.com>
2023-10-19 12:48:00 +0200

    Dynamic CSS style for bump animation upon cart add

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighLighted, setButtonIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
-    return currentNumber + item.amount;
+    return currentNumber + item.quantity;
   }, 0);
 
+  const buttonStyles = `${styles.button} ${
+    buttonIsHighLighted ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    console.log(buttonStyles);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
